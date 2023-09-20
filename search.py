ebe72828fba85e477474db6e48dd8d4a63299011
hamzacyberpatcher <hamzaoffuture@gmail.com>
2023-09-20 19:28:59 +0500

    Removed search.py

--- search.py
+++
@@ -1,9 +0,0 @@
-from cs50 import SQL
-
-db = SQL("sqlite:///pokemons.db")
-
-pokemon = input("Pokemon you want to search : ")
-
-pokemons = db.execute("SELECT name FROM pokemons WHERE evolves_from = ?",pokemon)
-
-print(pokemons)
